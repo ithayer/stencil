d371cdf8b133e4b4a4ad34ef8cdf7d60b9d4bd7e
Ignacio Thayer <ignacio@albacore.(none)>
2012-02-14 16:07:34 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject stencil "0.2.0"
-  :description "Mustache in Clojure"
+(defproject ithayer/stencil "0.2.0"
+  :description "Mustache in Clojure - modified to include c* context in lambdas"
   :dependencies [[clojure "1.3.0"]
                  [slingshot "0.8.0"]]
   :dev-dependencies [[ritz "0.2.0"]
